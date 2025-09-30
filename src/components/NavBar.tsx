2abfe1b3ff9858888b1e874343b815be14109fd8
RishiNandha Vanchi <rishirockzzmusic@gmail.com>
2025-09-30 07:13:37 +0530

    Fixes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,16 +4,18 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <NavLink to="/" className="nav-brand">Rishi Nandha Vanchinathan</NavLink>
-      <div className="nav-links">
-        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>About</NavLink>
-        <NavLink to="/cv" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>CV</NavLink>
-        <NavLink to="/projects" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Projects</NavLink>
+    <div className="navbar">
+      <nav className="navbar-content">
+        <NavLink to="/" className="nav-brand">Rishi Nandha Vanchinathan</NavLink>
+        <div className="nav-links">
+          <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>About</NavLink>
+          <NavLink to="/cv" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>CV</NavLink>
+          <NavLink to="/projects" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Projects</NavLink>
         <NavLink to="/music" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Music</NavLink>
         <NavLink to="/blog" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Blog</NavLink>
       </div>
     </nav>
+  </div>
   );
 };
 
