02218553aca87e124e536ba8fec39fb20e36869c
RishiNandha Vanchi <rishirockzzmusic@gmail.com>
2025-09-30 05:53:07 +0530

    Deployment

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,15 +3,10 @@
 const Projects = () => {
   return (
     <div>
-      <h1>My Projects</h1>
-      <p>Here are some of the projects I've worked on:</p>
-      <ul>
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-      </ul>
+      <h1>This section is still under construction</h1>
     </div>
   );
 };
+
 
 export default Projects;
