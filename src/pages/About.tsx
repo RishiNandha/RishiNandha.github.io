7317bb4764e7bde62b3ace4f283ad1a9d69a368a
RishiNandha Vanchi <rishirockzzmusic@gmail.com>
2025-10-03 15:12:48 +0530

    Update website

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaLinkedin, FaGithub, FaYoutube, FaEnvelope } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 // import profileGif from '../assets/profile.gif';
 import headshotJpg from '../assets/headshot.jpg';
 import './css/About.css';
@@ -14,13 +14,13 @@ const About = () => {
           <a href="https://www.linkedin.com/in/rishinandhav/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
           <a href="https://github.com/RishiNandha" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
           <a href="mailto:rishinandha.vanchi@gmail.com"><FaEnvelope /></a>
-          <a href="https://www.youtube.com/@rishinandha_vanchi" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+          {/* <a href="https://www.youtube.com/@rishinandha_vanchi" target="_blank" rel="noopener noreferrer"><FaYoutube /></a> */}
         </div>
         <p>I'm a final year student at IIT Madras (Integrated B.Tech + M.Tech) with experience in RF IC Design.</p>
         {/* { fss} */}
         <p>My research interests are analog in-memory and neuromorphic architectures. You can find some of my selected projects <Link to="/projects">here</Link>.</p>
         {/* <p>\n</p> */}
-        <p>I'm also a musician and have played some original works with my friends. You can find them <Link to="/music">here</Link>.</p>
+        <p>I'm also play the guitar at a semi-professional capacity. I have written some originals with my friends, some of which can be found <Link to="/music">here</Link>.</p>
       </div>
       <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
       <div className="about-right">
