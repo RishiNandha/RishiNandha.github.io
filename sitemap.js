a494bb6cfed3969b2fc47e78e4c7b45cee8fe065
RishiNandha Vanchi <rishirockzzmusic@gmail.com>
2026-03-31 13:40:45 +0530

    updated canonical

--- sitemap.js
+++ sitemap.js
@@ -24,13 +24,13 @@ let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>${baseUrl}/research</loc>
+    <loc>${baseUrl}/research/</loc>
     <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>${baseUrl}/blog</loc>
+    <loc>${baseUrl}/blog/</loc>
     <lastmod>${today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.85</priority>
@@ -40,7 +40,7 @@ let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 blogUrls.forEach((url) => {
   sitemap += `
   <url>
-    <loc>${baseUrl}/blog/${url}</loc>
+    <loc>${baseUrl}/blog/${url}/</loc>
     <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
@@ -49,13 +49,13 @@ blogUrls.forEach((url) => {
 
 sitemap += `
   <url>
-    <loc>${baseUrl}/music</loc>
+    <loc>${baseUrl}/music/</loc>
     <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
    <url>
-    <loc>${baseUrl}/activism</loc>
+    <loc>${baseUrl}/activism/</loc>
     <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
